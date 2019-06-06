b7403b1d702f701793b6d26c6851af01d0280770
Pawel Maciejewski <paulomac@o2.pl>
2019-06-06 19:22:26 +0200

    small updates

--- GeneratorThread/GeneratorWorker.cs
+++ GeneratorThread/GeneratorWorker.cs
@@ -1,7 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Common;
 using GeneratorThread.Interfaces;
+using Models;
+using Models.AddingThread;
 using Models.AddingThread.Enums;
+using RandomNameGeneratorLibrary;
+using Repository;
 
 namespace GeneratorThread
 {
@@ -10,19 +16,49 @@ namespace GeneratorThread
     /// </summary>
     public class GeneratorWorker : BaseWorker, IGeneratorWorker
     {
+        private readonly PersonNameGenerator personGenerator;
+
+        public GeneratorWorker()
+        {
+            personGenerator = new PersonNameGenerator();
+        }
+
         public override void Work()
         {
-            throw new NotImplementedException();
+            
         }
 
         public void GenerateClientsPocket()
         {
-            throw new NotImplementedException();
+            var pocekts = new List<Pocket>();
+
+            for (var i = 0; i < Settings.NumbersOfClients; i++)
+            {
+                var pocket = new Pocket
+                {
+                    OwnerName = personGenerator.GenerateRandomFirstAndLastName(),
+                    PrivateKey = Guid.NewGuid().ToString(),
+                    PublicKey = Guid.NewGuid().ToString()
+                };
+                pocekts.Add(pocket);
+            }
+
+            Datas.Pockets = pocekts;
         }
 
         public void CreateGenesisBlock()
         {
-            throw new NotImplementedException();
+            Datas.GenesisBlock = new Block
+            {
+                BlockHash = Guid.NewGuid().ToString(),
+                Data = new BlockData
+                {
+                    Amount = 50,
+                    Receiver = GetRandomClient().OwnerName
+                },
+                Hash = null, //generate hash?
+                PreviousBlockHash = string.Empty
+            };
         }
 
         public void GenerateProperTransfer()
@@ -33,6 +69,12 @@ namespace GeneratorThread
         public void GenerateUnproperTransaction(FakeTransactionType type)
         {
             throw new NotImplementedException();
+        }
+
+        private Pocket GetRandomClient()
+        {
+            var random = new Random(DateTime.Now.Millisecond);
+            return Datas.Pockets.ElementAt(random.Next(0, Settings.NumbersOfClients));
         }
     }
 }
---
+++ Repository/Datas.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Models;
+using Models.AddingThread;
+
+namespace Repository
+{
+    public static class Datas
+    {
+        public static IEnumerable<Pocket> Pockets { get; set; }
+        public static Block GenesisBlock { get; set; }
+    }
+}
